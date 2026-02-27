f570c65c74df5e26bce7d293c42b0769ff8e92d4
alexandergott-afk <alexander.gott@patentamt.at>
2026-02-27 12:10:26 +0100

    Update rfc2136.go
    Signed-off-by: alexandergott-afk <alexander.gott@patentamt.at>

--- pkg/issuer/acme/dns/rfc2136/rfc2136.go
+++ pkg/issuer/acme/dns/rfc2136/rfc2136.go
@@ -14,70 +14,62 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package rfc2136 implements a DNS provider for solving the DNS-01 challenge
-// using the rfc2136 dynamic update.
-// This code was adapted from lego:
-// 	  https://github.com/xenolf/lego
-
 package rfc2136
 
 import (
-	"fmt"
-	"strings"
-	"time"
+    "fmt"
+    "strings"
+    "time"
 
-	"github.com/miekg/dns"
+    "github.com/miekg/dns"
 
-	"github.com/cert-manager/cert-manager/internal/apis/certmanager/validation/util"
-	logf "github.com/cert-manager/cert-manager/pkg/logs"
+    "github.com/cert-manager/cert-manager/internal/apis/certmanager/validation/util"
+    logf "github.com/cert-manager/cert-manager/pkg/logs"
 )
 
 // This list must be kept in sync with internal/apis/certmanager/validation/issuer.go
 var supportedAlgorithms = map[string]string{
-	"HMACMD5":    dns.HmacMD5,
-	"HMACSHA1":   dns.HmacSHA1,
-	"HMACSHA256": dns.HmacSHA256,
-	"HMACSHA512": dns.HmacSHA512,
+    "HMACMD5":    dns.HmacMD5,
+    "HMACSHA1":   dns.HmacSHA1,
+    "HMACSHA256": dns.HmacSHA256,
+    "HMACSHA512": dns.HmacSHA512,
 }
 
 // DNSProvider is an implementation of the acme.ChallengeProvider interface that
 // uses dynamic DNS updates (RFC 2136) to create TXT records on a nameserver.
 type DNSProvider struct {
-	nameserver    []string
-	tsigAlgorithm string
-	network       string
-	tsigKeyName   string
-	tsigSecret    string
+    nameservers   []string // GEÄNDERT: Slice von Strings statt einzelner String
+    tsigAlgorithm string
+    network       string
+    tsigKeyName   string
+    tsigSecret    string
 }
 
 // ProviderOption is some configuration that modifies rfc2136 DNS provider.
-// It is meant to replace optional parameters and move towards function parameters and higher order functions.
-// Currently only protocol is migrated and future work is to be done on this.
 type ProviderOption func(*DNSProvider)
 
 func WithNetwork(network string) ProviderOption {
-	return func(d *DNSProvider) {
-		if network == "" {
-			network = "udp"
-		}
-		d.network = strings.ToLower(network)
-	}
+    return func(d *DNSProvider) {
+        if network == "" {
+            network = "udp"
+        }
+        d.network = strings.ToLower(network)
+    }
 }
 
 // NewDNSProviderCredentials uses the supplied credentials to return a
-// DNSProvider instance configured for rfc2136 dynamic update. To disable TSIG
-// authentication, leave the TSIG parameters as empty strings.
-// nameserver must be a network address in the form "IP" or "IP:port".
+// DNSProvider instance configured for rfc2136 dynamic update.
+// nameserver kann jetzt eine Komma-separierte Liste sein (z.B. "10.0.0.1,10.0.0.2").
 func NewDNSProviderCredentials(nameserver, tsigAlgorithm, tsigKeyName, tsigSecret string, opts ...ProviderOption) (*DNSProvider, error) {
-	logf.Log.V(logf.DebugLevel).Info("Creating RFC2136 Provider")
+    logf.Log.V(logf.DebugLevel).Info("Creating RFC2136 Provider")
 
-	d := &DNSProvider{}
+    d := &DNSProvider{}
 
-	for _, opt := range opts {
-		opt(d)
-	}
+    for _, opt := range opts {
+        opt(d)
+    }
 
-	// NEU: Splitte den String an Kommas, um mehrere Server zu unterstützen
+    // NEU: Splitte den String an Kommas, um mehrere Server zu unterstützen
     serverList := strings.Split(nameserver, ",")
     var validServers []string
 
@@ -93,74 +85,79 @@ func NewDNSProviderCredentials(nameserver, tsigAlgorithm, tsigKeyName, tsigSecre
         }
     }
 
-	if len(validServers) == 0 {
+    if len(validServers) == 0 {
         return nil, fmt.Errorf("no valid nameservers provided")
-    }else{
-		d.nameservers = validServers
-	}
+    }
 
-	if len(tsigKeyName) > 0 && len(tsigSecret) > 0 {
-		d.tsigKeyName = tsigKeyName
-		d.tsigSecret = tsigSecret
-	}
+    d.nameservers = validServers
 
-	if tsigAlgorithm == "" {
-		tsigAlgorithm = dns.HmacMD5
-	} else {
-		if value, ok := supportedAlgorithms[strings.ToUpper(tsigAlgorithm)]; ok {
-			tsigAlgorithm = value
-		} else {
-			return nil, fmt.Errorf("algorithm '%v' is not supported", tsigAlgorithm)
-		}
-	}
-	d.tsigAlgorithm = tsigAlgorithm
+    if len(tsigKeyName) > 0 && len(tsigSecret) > 0 {
+        d.tsigKeyName = tsigKeyName
+        d.tsigSecret = tsigSecret
+    }
 
-	keyLen := len(d.tsigSecret)
-	mask := make([]rune, keyLen/2)
-	for i := range mask {
-		mask[i] = '*'
-	}
-	masked := d.tsigSecret[0:keyLen/4] + string(mask) + d.tsigSecret[keyLen/4*3:keyLen]
-	logf.Log.V(logf.DebugLevel).Info("DNSProvider",
-		"nameserver", d.nameserver,
-		"tsigAlgorithm", d.tsigAlgorithm,
-		"tsigKeyName", d.tsigKeyName,
-		"tsigSecret", masked,
-	)
+    if tsigAlgorithm == "" {
+        tsigAlgorithm = dns.HmacMD5
+    } else {
+        if value, ok := supportedAlgorithms[strings.ToUpper(tsigAlgorithm)]; ok {
+            tsigAlgorithm = value
+        } else {
+            return nil, fmt.Errorf("algorithm '%v' is not supported", tsigAlgorithm)
+        }
+    }
+    d.tsigAlgorithm = tsigAlgorithm
 
-	return d, nil
+    // Logging (maskiert das Secret)
+    keyLen := len(d.tsigSecret)
+    mask := make([]rune, keyLen/2)
+    for i := range mask {
+        mask[i] = '*'
+    }
+    masked := ""
+    if keyLen > 0 {
+        masked = d.tsigSecret[0:keyLen/4] + string(mask) + d.tsigSecret[keyLen/4*3:keyLen]
+    }
+    logf.Log.V(logf.DebugLevel).Info("DNSProvider",
+        "nameservers", d.nameservers, // Loggt nun die Liste
+        "tsigAlgorithm", d.tsigAlgorithm,
+        "tsigKeyName", d.tsigKeyName,
+        "tsigSecret", masked,
+    )
+
+    return d, nil
 }
 
 // Present creates a TXT record using the specified parameters
 func (r *DNSProvider) Present(_, fqdn, zone, value string) error {
-	return r.changeRecord("INSERT", fqdn, zone, value, 60)
+    return r.changeRecord("INSERT", fqdn, zone, value, 60)
 }
 
 // CleanUp removes the TXT record matching the specified parameters
 func (r *DNSProvider) CleanUp(_, fqdn, zone, value string) error {
-	return r.changeRecord("REMOVE", fqdn, zone, value, 60)
+    return r.changeRecord("REMOVE", fqdn, zone, value, 60)
 }
 
 func (r *DNSProvider) changeRecord(action, fqdn, zone, value string, ttl uint32) error {
-	// Create RR
-	rr := new(dns.TXT)
-	rr.Hdr = dns.RR_Header{Name: fqdn, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: ttl}
-	rr.Txt = []string{value}
-	rrs := []dns.RR{rr}
+    // Create RR
+    rr := new(dns.TXT)
+    rr.Hdr = dns.RR_Header{Name: fqdn, Rrtype: dns.TypeTXT, Class: dns.ClassINET, Ttl: ttl}
+    rr.Txt = []string{value}
+    rrs := []dns.RR{rr}
 
-	// Create dynamic update packet
-	m := new(dns.Msg)
-	m.SetUpdate(zone)
-	switch action {
-	case "INSERT":
-		m.Insert(rrs)
-	case "REMOVE":
-		m.Remove(rrs)
-	default:
-		return fmt.Errorf("unexpected action: %s", action)
-	}
+    // Create dynamic update packet
+    m := new(dns.Msg)
+    m.SetUpdate(zone)
+    switch action {
+    case "INSERT":
+        m.Insert(rrs)
+    case "REMOVE":
+        m.Remove(rrs)
+    default:
+        return fmt.Errorf("unexpected action: %s", action)
+    }
 
-	var lastErr error
+    // NEU: Iteriere über alle Nameserver und sende das Update
+    var lastErr error
     for _, ns := range r.nameservers {
         // Setup client (muss pro Request neu erstellt oder resettet werden, ist aber hier sicherer so)
         c := &dns.Client{Net: r.network}
@@ -192,6 +189,11 @@ func (r *DNSProvider) changeRecord(action, fqdn, zone, value string, ttl uint32)
         }
     }
 
+    // Wenn auch nur ein Fehler aufgetreten ist, geben wir diesen zurück.
+    // So stellt cert-manager sicher, dass nicht "Erfolg" gemeldet wird, wenn ein Server down ist.
+    // Zertifikat-Erstellung wird fehlschlagen, wenn nicht alle Server erreicht werden konnten.
+    // Alternativ könnte man hier nur fehler zurückgeben, wenn ALLE fehlschlugen.
+    // Für ACME ist es sicherer, Fehler zu melden, wenn inkonsistente Zustände drohen.
     return lastErr
 }
 
@@ -206,5 +208,5 @@ func (r *DNSProvider) Nameserver() string {
 
 // TSIGAlgorithm returns the TSIG algorithm configured for this provider when it was created
 func (r *DNSProvider) TSIGAlgorithm() string {
-	return r.tsigAlgorithm
+    return r.tsigAlgorithm
 }

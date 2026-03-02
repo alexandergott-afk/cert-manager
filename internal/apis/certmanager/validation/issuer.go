2988abbfd9ab61105835127149dc24745b606081
alexandergott-afk <alexander.gott@patentamt.at>
2026-03-02 11:08:47 +0100

    Update issuer.go
    Signed-off-by: alexandergott-afk <alexander.gott@patentamt.at>

--- internal/apis/certmanager/validation/issuer.go
+++ internal/apis/certmanager/validation/issuer.go
@@ -594,10 +594,12 @@ func ValidateACMEChallengeSolverDNS01(p *cmacme.ACMEChallengeSolverDNS01, fldPat
 			numProviders++
 			// Nameserver is the only required field for RFC2136
 			if len(p.RFC2136.Nameserver) == 0 {
-				el = append(el, field.Required(fldPath.Child("rfc2136", "nameserver"), ""))
+				el = append(el, field.Required(fldPath.Child("rfc2136", "nameservers"), ""))
 			} else {
-				if _, err := util.ValidNameserver(p.RFC2136.Nameserver); err != nil {
-					el = append(el, field.Invalid(fldPath.Child("rfc2136", "nameserver"), p.RFC2136.Nameserver, "nameserver must be set in the form host:port where host is an IPv4 address, an enclosed IPv6 address or a hostname and port is an optional port number."))
+				for _, ns := range p.RFC2136.Nameservers {
+					if _, err := util.ValidNameserver(ns); err != nil {
+						el = append(el, field.Invalid(fldPath.Child("rfc2136", "nameservers"), ns, "nameserver must be set in the form host:port where host is an IPv4 address, an enclosed IPv6 address or a hostname and port is an optional port number."))
+					}
 				}
 			}
 			if len(p.RFC2136.TSIGAlgorithm) > 0 {
